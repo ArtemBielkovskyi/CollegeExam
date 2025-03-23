fcd51650d21839f3b8ff6f0775a4e4904dd082a3
ArtemBielkovskyi <157801683+ArtemBielkovskyi@users.noreply.github.com>
2025-03-23 10:54:59 +0000

    Added index files, to build header on the website

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Header</title>
+        <link rel="stylesheet" href="index.css">
+    </head>
+    <body>
+        <div class="header">
+            <h2 class="name">Rolsa Technologies</h2>
+            <div class="button">About us</div>
+            <div class="button">Carbon footprint</div>
+            <div class="button">Products</div>
+            <div class="button">Contact us</div>
+            <div class="button">Registration/login</div>
+        </div>
+    </body>
+</html>
